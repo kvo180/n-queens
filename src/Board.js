e3f37bf83a42e849a87e950b485ec464cc852da7
Hack Reactor Student <student@hackreactor.com>
2017-04-05 17:37:06 -0700

    Implement majorDiagonals and part of minorDiags

--- src/Board.js
+++ src/Board.js
@@ -137,12 +137,29 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var count = 0;
+      var rowIdx = 0;
+      var colIdx = majorDiagonalColumnIndexAtFirstRow;
+      var size = this.get('n');
+      var rows = this.rows();
+      for (rowIdx, colIdx; rowIdx < size && colIdx < size; rowIdx++, colIdx++) {
+        if ( rowIdx >= 0 && colIdx >= 0) {
+          count += rows[rowIdx][colIdx];
+        }
+      }
+      return count > 1;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      var size = this.get('n');
+
+      for (var i = -size; i < size; i++) {
+        if (this.hasMajorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -152,12 +169,29 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var count = 0;
+      var rowIdx = 0;
+      var colIdx = minorDiagonalColumnIndexAtFirstRow;
+      var size = this.get('n');
+      var rows = this.rows();
+      for (rowIdx, colIdx; rowIdx < size && colIdx >= 0; rowIdx++, colIdx--) {
+        if (rowIdx >= 0 && colIdx >= 0) {
+          count += rows[rowIdx][colIdx];
+        }
+      }
+      return count > 1;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var size = this.get('n');
+
+      for (var i = size * 2; i >= 0; i--) {
+        if (this.hasMinorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
